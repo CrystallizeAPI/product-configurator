e49890f54594b2d89fb966dd1cc8141bd4754d2b
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-01-10 14:46:06 +0200

    Change default font family to Roboto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,33 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+    weight: ["400", "500"],
+    subsets: ["latin"],
+    display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: 'Product Configurator ',
-  description: 'Product Configurator Boilerplate powerd by Crystallize',
-}
+    title: "Product Configurator ",
+    description: "Product Configurator Boilerplate powered by Crystallize",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <head>
-        <script async 
-          type="module"
-        src="https://cdn2.charpstar.net/ConfigFiles/Crystallize/SpeedCurve/mvc.js" />
-      </head>
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <head>
+                <script
+                    async
+                    type="module"
+                    src="https://cdn2.charpstar.net/ConfigFiles/Crystallize/SpeedCurve/mvc.js"
+                />
+            </head>
+            <body className={roboto.className}>{children}</body>
+        </html>
+    );
 }
