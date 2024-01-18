f078e85ef7aa1ce96dadca49e02dd58ffcf62af8
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2024-01-18 09:10:57 +0200

    Add number of items in the shopping bag

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { useCartContext } from "@/context/cart-context";
+import { CART_ID } from "@/utils/const";
 import Link from "next/link";
 
 export function Header() {
     const { setIsCartOpen } = useCartContext();
+    const hasCartId = !!localStorage.getItem(CART_ID);
 
     return (
         <header className="bg-white px-12 py-4 flex border-b justify-between items-center">
@@ -33,7 +35,7 @@ export function Header() {
 
             <button
                 type="button"
-                className="p-2 cursor-pointer pr-0"
+                className="p-2 cursor-pointer pr-0 relative"
                 onClick={() => setIsCartOpen?.(true)}
             >
                 <svg
@@ -44,6 +46,11 @@ export function Header() {
                 >
                     <path d="M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z" />
                 </svg>
+                {hasCartId && (
+                    <div className="absolute grid place-content-center w-5 h-5 top-4 left-3.5 text-gray-800 font-medium text-xs">
+                        1
+                    </div>
+                )}
             </button>
         </header>
     );
