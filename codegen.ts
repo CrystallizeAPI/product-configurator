84e83d49b39085bad6cb9815035ccd19952e16ef
Vasil Dimitrov <v.y.dimitrov@gmail.com>
2025-02-06 13:24:18 +0200

    Fix codegen

--- codegen.ts
+++ codegen.ts
@@ -1,18 +1,18 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-    schema: 'https://api.crystallize.com/furnitut/discovery',
-    documents: ['app/**/*.graphql', 'components/**/*.graphql'],
-    ignoreNoDocuments: true,
     overwrite: true,
-    config: {
-        avoidOptionals: true,
-    },
+    schema: [
+        {
+            [`https://api.crystallize.com/product-configurator/catalogue`]: {},
+        },
+    ],
     generates: {
-        'generated/': {
-            preset: 'client',
-            presetConfig: { fragmentMasking: false },
-            plugins: [{ add: { content: '//@ts-nocheck' } }],
+        './src/use-cases/crystallize/__generated__/types.ts': {
+            plugins: ['typescript'],
+            config: {
+                preResolveTypes: false,
+            },
         },
     },
 };
