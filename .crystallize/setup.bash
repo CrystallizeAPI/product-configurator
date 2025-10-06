fded35dbd3dadc9fa82a5c41000a12c6e98ac18b
Morel Sébastien <morel.seb@gmail.com>
2025-10-06 12:46:32 -0700

    feat: config the auto installer content model and minimum item set

---
+++ .crystallize/setup.bash
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "${SCRIPT}")
+PROJECT_PATH=${SCRIPTPATH}/..
+
+if [ ! -d "${PROJECT_PATH}/.crystallize" ]; then
+    echo "It does not seem to be a clean clone. Aborting."
+    exit 1
+fi
+
+# Note that Crystallize CLI is acting before this script.
+
+echo "Setup ${PROJECT_PATH}"
+cp ${PROJECT_PATH}/.crystallize/env ${PROJECT_PATH}/.env
+cp ${PROJECT_PATH}/.crystallize/robots.txt ${PROJECT_PATH}/public/robots.txt
+
+#---
+
+echo "Running command ${PROJECT_PATH}"
+
+if ! command -v pnpm &> /dev/null; then
+    cd ${PROJECT_PATH} && npm install --no-progress --no-color
+    cd ${PROJECT_PATH} && npm run codegen
+else 
+    cd ${PROJECT_PATH} && pnpm install --silent --no-color
+    cd ${PROJECT_PATH} && npm run codegen
+fi
+#---
+
+echo "Cleanup ${PROJECT_PATH}"
+
+rm -rf ${PROJECT_PATH}/.github
+rm -rf ${PROJECT_PATH}/.git
+rm -rf ${PROJECT_PATH}/.vscode
+rm -rf ${PROJECT_PATH}/.crystallize
--- src/core/credentials.ts
+++ src/core/credentials.ts
@@ -2,6 +2,7 @@
 
 export const credentials = {
     tenantIdentifier: `${process.env.CRYSTALLIZE_TENANT_IDENTIFIER}`,
+    tenantId: `${process.env.CRYSTALLIZE_TENANT_ID}`,
     accessTokenId: `${process.env.CRYSTALLIZE_ACCESS_TOKEN_ID}`,
     accessTokenSecret: `${process.env.CRYSTALLIZE_ACCESS_TOKEN_SECRET}`,
 } as const;
